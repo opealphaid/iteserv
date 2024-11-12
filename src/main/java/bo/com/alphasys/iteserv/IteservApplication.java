7385520d61385c5b51212e79999da24c7211c349
Carlos Peralta <cperalta@alphasys.com.bo>
2024-11-12 15:25:39 -0400

    [ADD] Initial Commit

---
+++ src/main/java/bo/com/alphasys/iteserv/IteservApplication.java
@@ -0,0 +1,13 @@
+package bo.com.alphasys.iteserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteservApplication.class, args);
+	}
+
+}
